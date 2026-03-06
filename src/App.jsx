18b8791171e12f5bc20b790b88dfe370202e6b08
Sven Schirmer <sven.glueckspilz@mastersong.de>
2026-03-06 17:09:02 +0100

    simplified routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import Layout from './modules/Layout'
 import Home from './modules/Home'
 import About from './modules/About'
 import NoPage from './modules/NoPage'
 import { Routes } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import './App.css'
+import Navigation from './modules/Navigation'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
+    <div className="main">
+      <Navigation />
+      <Routes>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="*" element={<NoPage />} />
-      </Route>
-    </Routes>
+        <Route path="*" element={<NoPage />} />   
+      </Routes>
+    </div>
   )
-
 }
 
 export default App
